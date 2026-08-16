c9fe62c92e60e87bc40f9b2ccf18030b93396a7d
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2026-08-16 17:49:10 +0800

    Initial Commit

---
+++ documents/documentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  documentsApp.swift
+//  documents
+//
+//  Created by Joseph Kevin Fredric on 16/8/26.
+//
+
+import SwiftUI
+
+@main
+struct documentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
